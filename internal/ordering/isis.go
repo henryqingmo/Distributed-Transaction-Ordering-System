8fc5a7f28de9b79c2b4eee222bf18c06a8af7068
henryqingmo <henryqingmo@gmail.com>
2026-03-13 14:31:40 -0500

    Completed implementation for holdbackQueue

--- internal/ordering/isis.go
+++ internal/ordering/isis.go
@@ -2,24 +2,52 @@ package ordering
 
 import (
 	manager "cs425_mp1/internal/network"
+	"sort"
 )
 
 type Queue struct {
-	items []int
+	items []*QueueItem
+}
+
+type QueueItem struct {
+	id          string
+	tx          string
+	priority    float32
+	deliverable bool
+	sender      string
 }
 
 type isisOrdering struct {
-	queue Queue
+	holdbackQueue Queue
+	messageMap    map[string]*QueueItem
 }
 
-func (q *Queue) Enqueue(x int) {
-	q.items = append(q.items, x)
+func (q *Queue) Enqueue(item *QueueItem) {
+	q.items = append(q.items, item)
+	q.Sort()
 }
 
-func (q *Queue) Dequeue() int {
+func (q *Queue) Dequeue() *QueueItem {
 	x := q.items[0]
 	q.items = q.items[1:]
 	return x
+}
+
+func (q *Queue) Peek() *QueueItem {
+	if len(q.items) == 0 {
+		return nil
+	}
+	return q.items[0]
+}
+
+func NewQueueItem(id string, tx string, priority float32, deliverable bool, sender string) *QueueItem {
+	return &QueueItem{
+		id:          id,
+		tx:          tx,
+		priority:    priority,
+		deliverable: deliverable,
+		sender:      sender,
+	}
 }
 
 func (o *isisOrdering) HandleMessage(msg manager.Message) {
@@ -34,7 +62,21 @@ func (o *isisOrdering) HandleMessage(msg manager.Message) {
 
 }
 
+func (q *Queue) Sort() {
+	sort.Slice(q.items, func(i, j int) bool {
+		if q.items[i].priority == q.items[j].priority {
+			return q.items[i].sender < q.items[j].sender
+		}
+		return q.items[i].priority < q.items[j].priority
+	})
+}
+
 func (o *isisOrdering) OnReceiveTransaction(msg manager.Message) {
+	if msg.Transaction.Kind == manager.Deposit {
+	}
+
+	if msg.Transaction.Kind == manager.Transfer {
+	}
 
 }
 
