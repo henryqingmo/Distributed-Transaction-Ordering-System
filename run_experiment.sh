139861e93b492f926022883e372dcd151ace8c05
henryqingmo <henryqingmo@gmail.com>
2026-03-15 23:54:57 -0500

    update script to include custom timeout value for experiemnt

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# run_experiment.sh <nodeID> [hz]
-# Example: ./run_experiment.sh node1 0.5
+# run_experiment.sh <nodeID> [hz] [timeout]
+# Example: ./run_experiment.sh node1 0.5 100
+
+NODE=$1
 HZ=${2:-0.5}
-python3 -u gentx.py $HZ | timeout 100 ./mp1_node $1 config.txt
-echo "Done. Latency saved to latency_$1.txt"
+TIME=${3:-100}
+
+python3 -u gentx.py $HZ | timeout $TIME ./mp1_node $NODE config.txt
+
+echo "Done. Latency saved to latency_$NODE.txt"
